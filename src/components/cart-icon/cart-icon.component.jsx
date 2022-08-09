abafb9ccc01dbd3cc142f201920cf53825a86e77
Darijan <you@example.com>
2022-08-09 10:40:08 +0200

    addTOCart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,13 +4,18 @@ import { CartContext } from '../../contexts/cart.context';
 
 import { ReactComponent as ShoppingCartIcon } from '../../assets/shopping-bag.svg'
 
-const CartIcon = () => {const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+const CartIcon = () => {
+    
+const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+
+const {cartQuantity} = useContext(CartContext);
+const {cartItems} = useContext(CartContext);
 
 const toggleCartOpen = () => setIsCartOpen(!isCartOpen);
     return(
         <div className='cart-icon-container' onClick={toggleCartOpen}>
             <ShoppingCartIcon className='shopping-icon'/>
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartItems.length? cartItems.map(item=> item.quantity).reduce((a,b)=> a + b) : 0}</span>
         </div>
     )
 }
